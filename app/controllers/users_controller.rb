3980da2393d6969ff7f2c23c76a6cf71688b25c7
Dr. Funkenstein <lauherk@gmail.com>
2012-04-11 10:44:13 +0900

    Make a basic User model(including encrypted passwords)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def new
   end
+   rescue ActiveRecord::StatementInvalid
+    # Handle duplicate email addresses gracefully by redirecting.
+    redirect_to home_url
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,18 +6,18 @@ describe "Static pages" do
 
     it "should have the h1 'Sample App'" do
       visit root_path
-      page.should have_selector('h1', text: 'Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the base title" do
       visit root_path
       page.should have_selector('title',
-                        text: "Ruby on Rails Tutorial Sample App")
+                        :text => "Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
       visit root_path
-      page.should_not have_selector('title', text: '| Home')
+      page.should_not have_selector('title', :text =>'| Home')
     end
   end
 
@@ -25,13 +25,13 @@ describe "Static pages" do
 
     it "should have the h1 'Help'" do
       visit help_path
-      page.should have_selector('h1', text: 'Help')
+      page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
       visit help_path
       page.should have_selector('title',
-                        text: "Ruby on Rails Tutorial Sample App | Help")
+                        :text =>  "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
@@ -39,13 +39,13 @@ describe "Static pages" do
 
     it "should have the h1 'About'" do
       visit about_path
-      page.should have_selector('h1', text: 'About Us')
+      page.should have_selector('h1', :text =>  'About Us')
     end
 
     it "should have the title 'About Us'" do
       visit about_path
       page.should have_selector('title',
-                    text: "Ruby on Rails Tutorial Sample App | About Us")
+                    :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 
@@ -53,13 +53,13 @@ describe "Static pages" do
 
     it "should have the h1 'Contact'" do
       visit contact_path
-      page.should have_selector('h1', text: 'Contact')
+      page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the title 'Contact'" do
       visit contact_path
       page.should have_selector('title',
-                    text: "Ruby on Rails Tutorial Sample App | Contact")
+                    :text => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
