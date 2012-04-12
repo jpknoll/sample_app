16a0ad06d3bd51663b51ebdcd0c36a2ec9641e04
Dr. Funkenstein <lauherk@gmail.com>
2012-04-12 14:51:00 +0900

    Finish User Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+    def create
+    @user = User.new(params[:user])
+    if @user.save
+    	 flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
    rescue ActiveRecord::StatementInvalid
     # Handle duplicate email addresses gracefully by redirecting.
     redirect_to home_url
